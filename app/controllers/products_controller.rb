d712dd22342b743aebb2dcd4c4f95b753982864c
github username <github email address>
2019-09-17 16:31:31 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,11 +13,6 @@ class ProductsController < ApplicationController
     render plain: product.description
   end
 
-  def data
-    product = Product.find(params[:id])
-    render json: ProductSerializer.serializer(product)
-  end
-
   def new
     @product = Product.new
   end
@@ -29,6 +24,11 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   private
